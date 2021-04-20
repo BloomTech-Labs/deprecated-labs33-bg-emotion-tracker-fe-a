ad44f2e500d0b0b19288e1d6657a3637c857d74a
Christopher Corvo <christophercorvo@gmail.com>
2021-04-20 17:40:12 -0400

    adds a header to the CSV drag and drop component

--- src/components/pages/member-dash-modal/CsvDragAndDrop.js
+++ src/components/pages/member-dash-modal/CsvDragAndDrop.js
@@ -2,29 +2,38 @@ import React from 'react';
 import { Upload, message } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
 
-import axios from 'axios';
-import { axiosWithAuth } from './axioswithauth';
+import { getAuthHeader } from '../../../api';
+
+// 4-20-21
+import { useOktaAuth } from '@okta/okta-react';
 
 function CsvDragAndDrop() {
   const { Dragger } = Upload;
 
-  let customRequest = () => {
-    // inside this function will be the axios call
-    axiosWithAuth()
-      .post('/members/upload')
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(error => {
-        console.log(error.response);
-      });
-  };
+  // let customRequest = () => {
+  //   // inside this function will be the axios call
+  //   axiosWithAuth()
+  //     .post('/members/upload')
+  //     .then(res => {
+  //       console.log(res.data);
+  //     })
+  //     .catch(error => {
+  //       console.log(error.response);
+  //     });
+  // };
+
+  const { authState, oktaAuth } = useOktaAuth();
+
+  const token = getAuthHeader(authState);
+  console.log(token);
 
   const props = {
     name: 'file',
+    action: 'https://bg-emotion-tracker-be-a.herokuapp.com/members/upload',
+    headers: token,
     onChange(info) {
       const { status } = info.file;
-      console.log(info.file.getItem);
+
       if (status !== 'uploading') {
         console.log(info.file, info.fileList);
       }
@@ -33,7 +42,6 @@ function CsvDragAndDrop() {
       } else if (status === 'error') {
         message.error(`${info.file.name} file upload failed.`);
       }
-      console.log(info.file.fileList);
     },
   };
 
