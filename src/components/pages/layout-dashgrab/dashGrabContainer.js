e6868b09ec044b92bf260ccc13d3a34a33416ff5
ginabethrussell <ginabeth.russell@gmail.com>
2021-04-21 10:58:02 -0400

    adds useEffect to call backend api to get loggedin userdata/role

--- src/components/pages/layout-dashgrab/dashGrabContainer.js
+++ src/components/pages/layout-dashgrab/dashGrabContainer.js
@@ -1,42 +1,59 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
-import { dashGrab } from '.';
+import { dashGrab as DashGrab } from '.';
+import { getUserData } from '../../../api';
 
-function DashGrabContainer ({ LoadingComponent }) {
-    const { authState, authService } = useOktaAuth();
-    const [userInfo, setUserInfo] = useState(null);
-    // eslint-disable-next-line
-    const [memoAuthService] = useMemo(() => [authService], []);
+function DashGrabContainer({ LoadingComponent }) {
+  const { authState, authService } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState(null);
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
-    useEffect(() => {
-        let isSubscribed = true;
-    
-        memoAuthService
-          .getUser()
-          .then(info => {
-            // if user is authenticated we can use the authService to snag some user info.
-            // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
-            if (isSubscribed) {
-              setUserInfo(info);
-            }
-          })
-          .catch(err => {
-            isSubscribed = false;
-            return setUserInfo(null);
-          });
-        return () => (isSubscribed = false);
-      }, [memoAuthService]);
+  // eslint-disable-next-line
+  const [memoAuthService] = useMemo(() => [authService], []);
 
-    return (
-        <>
-        {authState.isAuthenticated && !userInfo && (
-            <LoadingComponent message="Fetching user profile..." />
-        )}
-        {authState.isAuthenticated && userInfo && (
-            <dashGrab userInfo={userInfo} authService={authService} />
-        )}
-        </>
-    );
-};
+  useEffect(() => {
+    let isSubscribed = true;
+
+    memoAuthService
+      .getUser()
+      .then(info => {
+        // if user is authenticated we can use the authService to snag some user info.
+        // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
+        if (isSubscribed) {
+          setUserInfo(info);
+        }
+      })
+      .catch(err => {
+        isSubscribed = false;
+        return setUserInfo(null);
+      });
+    return () => (isSubscribed = false);
+  }, [memoAuthService]);
+
+  useEffect(() => {
+    if (userInfo) {
+      getUserData('/users/getuserinfo', authState)
+        .then(res => {
+          setLoggedInUser(res);
+        })
+        .catch(err => console.log(err));
+    }
+  }, [userInfo]);
+
+  return (
+    <>
+      {authState.isAuthenticated && !userInfo && !loggedInUser && (
+        <LoadingComponent message="Fetching user profile..." />
+      )}
+      {authState.isAuthenticated && userInfo && loggedInUser && (
+        <DashGrab
+          userInfo={userInfo}
+          authService={authService}
+          loggedInUser={loggedInUser}
+        />
+      )}
+    </>
+  );
+}
 
 export default DashGrabContainer;
