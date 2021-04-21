bb85e7447c59ac291efeb40f73eaeac036e95245
ginabethrussell <ginabeth.russell@gmail.com>
2021-04-21 11:00:34 -0400

    adds role prop and renderSwitch to render correct content on menu click

--- src/components/pages/layout-navs/SuperAdminNav.jsx
+++ src/components/pages/layout-navs/SuperAdminNav.jsx
@@ -1,49 +1,94 @@
 import { Avatar, Layout, Menu } from 'antd';
-import Sider from 'antd/lib/layout/Sider';
-import React from 'react';
+// import Sider from 'antd/lib/layout/Sider';
+import React, { useState } from 'react';
 import { UserOutlined, BookFilled, BookOutlined } from '@ant-design/icons';
+import { useOktaAuth } from '@okta/okta-react';
+import MemberTable from '../view-memberdash/MemberTable';
 
-const SuperAdminNav = ({ props }) => {
-    const { authService } = props;
-    return(
-        <>
-        <Layout>
-            <Sider
-            breakpoint="lg"
-            width="250"
-            collapsedWidth="0"
-            onBreakpoint={broken => {
-                console.log(broken);
-            }}
-            onCollapse={(collapsed, type) => {
-                console.log(collapsed, type);
-            }}
+const SuperAdminNav = props => {
+  const { authService } = useOktaAuth();
+  const { Content, Sider } = Layout;
+  const [content, setContent] = useState('');
+  const role = props.role;
+
+  const renderSwitch = contentType => {
+    switch (contentType) {
+      case 'members':
+        return <MemberTable />;
+      // case("programs"):
+      //     return <ProgramTable />
+      // case("clubs"):
+      //     return <ClubTable />
+      case '':
+        return <div></div>;
+      default:
+        return <div></div>;
+    }
+  };
+  return (
+    <>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          breakpoint="lg"
+          width="250"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}>
+            <Avatar size={100} gap="5" icon={<UserOutlined />} />
+            <Menu.Item
+              key="0"
+              icon={<UserOutlined />}
+              onClick={() => setContent('')}
             >
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}>
-                    <Avatar size={100} gap="5" icon={<UserOutlined />} />
-                    <Menu.Item key="1" icon={<UserOutlined />}>
-                    Dashboard
-                    </Menu.Item>
-                    <Menu.Item key="1" icon={<UserOutlined />}>
-                    Program Management
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<BookOutlined />}>
-                    Member Management
-                    </Menu.Item>
-                    <Menu.Item key="3" icon={<BookFilled />}>
-                    Club Management
-                    </Menu.Item>
-                    <Menu.Item key="4" icon={<UserOutlined />}>
-                    QR Generator
-                    </Menu.Item>
-                    <Menu.Item key="5" onClick={() => authService.logout()}>
-                    Logout
-                    </Menu.Item>
-                </Menu>
-            </Sider>
+              {role} Dashboard
+            </Menu.Item>
+            <Menu.Item
+              key="1"
+              icon={<UserOutlined />}
+              onClick={() => setContent('programs')}
+            >
+              Program Management
+            </Menu.Item>
+            <Menu.Item
+              key="2"
+              icon={<BookOutlined />}
+              onClick={() => setContent('members')}
+            >
+              Member Management
+            </Menu.Item>
+            <Menu.Item
+              key="3"
+              icon={<BookFilled />}
+              onClick={() => setContent('clubs')}
+            >
+              Club Management
+            </Menu.Item>
+            <Menu.Item
+              key="4"
+              icon={<UserOutlined />}
+              onClick={() => setContent('qrcodes')}
+            >
+              QR Generator
+            </Menu.Item>
+            <Menu.Item key="5" onClick={() => authService.logout()}>
+              Logout
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content style={{ margin: '0 16px' }}>
+            <>{renderSwitch(content)}</>
+          </Content>
         </Layout>
-        </>
-    );
+      </Layout>
+    </>
+  );
 };
 
 export default SuperAdminNav;
