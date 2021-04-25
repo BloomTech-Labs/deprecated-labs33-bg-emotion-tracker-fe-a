6e77f5b67a682deab1209eedb0cd3668ded74a3c
ginabethrussell <ginabeth.russell@gmail.com>
2021-04-24 20:29:14 -0400

    adds css and button to print cards

--- src/components/pages/view-memberdash/new-modal-member/QRCodeRender.js
+++ src/components/pages/view-memberdash/new-modal-member/QRCodeRender.js
@@ -1,34 +1,56 @@
 import React from 'react';
 import QRCode from 'qrcode.react';
-import PDFcomponent from './PDFcomponent';
-import { PDFDownloadLink, Page } from '@react-pdf/renderer';
+import { Button } from 'antd';
+import logo from '../../layout-navs/BGC_logo2.png';
 
 const QRCodeGenerator = props => {
-  const downloadName = 'qrcodes';
   const { newMemberId } = props;
 
   return (
-    <div className="App">
-      <Page>
-        {newMemberId.map(memberId => (
-          <div>
-            <QRCode
-              value={memberId}
-              renderAs="svg"
-              fgColor="#333"
-              bgColor="#fff"
-              key={`level-${memberId}`}
-            />
-            <h2>{memberId}</h2>
+    <div
+      style={{
+        margin: '100px',
+      }}
+    >
+      <Button size="large" type="primary" onClick={() => window.print()}>
+        Print IDs
+      </Button>
+
+      {newMemberId.map(memberId => (
+        <div
+          style={{
+            // Pixel dimensions for a credit card size card
+            width: '324px',
+            height: '204px',
+            borderStyle: 'dashed',
+            borderColor: 'gray',
+            margin: '25px',
+            padding: 20,
+            pageBreakBefore: 'always',
+          }}
+        >
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <img src={logo} height="75px" alt="logo" />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <QRCode
+                value={memberId}
+                renderAs="svg"
+                fgColor="#333"
+                bgColor="#fff"
+                key={`level-${memberId}`}
+              />
+              <h2>{memberId}</h2>
+            </div>
           </div>
-        ))}
-      </Page>
-      <PDFDownloadLink
-        document={<PDFcomponent PDFImageIds={newMemberId} />}
-        fileName={`${downloadName}.pdf`}
-      >
-        {({ loading }) => (loading ? 'Loading document...' : 'Download PDF')}
-      </PDFDownloadLink>
+        </div>
+      ))}
     </div>
   );
 };
